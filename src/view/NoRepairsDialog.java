6b8cb3aa869d887fa69e9616d5a852188980d037
Tony Ehlert <aehlert@dmacc.edu>
2023-03-27 08:29:44 -0500

    Create NoRepairsDialog.java

---
+++ src/view/NoRepairsDialog.java
@@ -0,0 +1,86 @@
+package view;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+/*****************************************************************
+ * Name				: CIS152FinalProjectEhlert
+ * Author			: Tony Ehlert
+ * Created			: Mar 27, 2023
+ * Course			: CIS152 Data Structures
+ * Version			: 1.0
+ * OS				: Windows 11
+ * Copyright		: This is my own original work based on
+ *         	  	  	  specifications issued by our instructor
+ * Description		: This program serves as a final project for CIS152 Data Structures.
+ * 					  It is meant to help a service manager assign repairs in the proper
+ * 					  order.  It also contains a GUI with buttons to assign repairs, complete
+ * 					  repairs, and view different reports
+ *					 Input: Required information needed to create various technician objects as
+ *							well as the information needed to create various repair job objects.
+ *					 Output: Window displaying the next repair job along with the technician assigned to it.
+ *							 Various reports displaying jobs not started, jobs in progress, and completed jobs.
+ * Academic Honesty	: I attest that this is my original work.
+ * I have not used unauthorized source code, either modified or 
+ * unmodified. I have not given other fellow student(s) access to
+ * my program.         
+ *****************************************************************/
+public class NoRepairsDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			NoRepairsDialog dialog = new NoRepairsDialog();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public NoRepairsDialog() {
+		setBounds(100, 100, 450, 208);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			JLabel lblNewLabel = new JLabel("No Repairs Available");
+			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			lblNewLabel.setBounds(131, 61, 172, 14);
+			contentPanel.add(lblNewLabel);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+		}
+	}
+
+}
