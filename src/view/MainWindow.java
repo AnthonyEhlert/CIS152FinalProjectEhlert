cbd84ddd2319099d6dbaaf5d4654c79c260cfc00
Tony Ehlert <aehlert@dmacc.edu>
2023-03-27 13:03:16 -0500

    Update MainWindow.java
    Added JOptionPane.showMessageDialog objects for errors, instead of windows.
    
    Commented out print statements used for troubleshooting

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -10,6 +10,7 @@ import java.util.Queue;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
@@ -40,80 +41,6 @@ import model.Technician;
  * my program.         
  *****************************************************************/
 public class MainWindow extends JFrame {
-
-	
-//	private JPanel mainPane;
-//	private JButton btnAssignNextJob;
-//	private JButton btnCompleteRepair;
-	
-	/// CREATION OF GLOBAL VARIABLES/OBJECTS TO BE USED IN GUI \\\
-	
-	// TECHNICIAN OBJECTS & DATA STRUCTURE
-	// Queue to store technicians
-//	static Queue<Technician> techQ = new LinkedList<Technician> ();
-//
-//	// create technician objects
-//	static Technician tech1 = new Technician(1, "Tony", "Stark");
-//	static Technician tech2 = new Technician(2, "Steve", "Rodgers");
-//	static Technician tech3 = new Technician(3, "Thor", "Odinson");
-//	static Technician tech4 = new Technician(4, "Bruce", "Banner");
-//	static Technician tech5 = new Technician(5, "Natasha", "Romanoff");
-//	static Technician tech6 = new Technician(6, "Clint", "Barton");
-//	
-//	// REPAIR OBJECTS & DATA STRUCTURE
-//	// create PriorityQueue to store repairs not started
-//	static PriorityQueue<Repair> repairsNotStartedQ = new PriorityQueue<Repair>();
-//	
-//	// creation of empty linked lists to store inProgress repairs and completed repairs
-//	static LinkedList<Repair> inProgressList = new LinkedList<Repair>();
-//	static LinkedList<Repair> completedList = new LinkedList<Repair>();
-//	
-//	// creation of repair objects
-//	static Repair repair1 = new Repair(1, 4, "Shield Inc.");
-//	static Repair repair2 = new Repair(2, 2, "Hydra Inc.");
-//	static Repair repair3 = new Repair(3, 3, "Stark Enterprises");
-//	static Repair repair4 = new Repair(4, 1, "Avengers");
-//	
-//	// create Repair objects with a priority value outside of 1-4
-//	static Repair repair5 = new Repair(5, 0, "USA Electronics");
-//	static Repair repair6 = new Repair(6, 5, "Ag Electronics LLC");
-//	
-//	// create Repair object with orderNum == 0
-//	static Repair repair0 = new Repair(0, 2, "Dark Order");
-//	
-//	// create Repair object with orderNum < 0
-//	static Repair repairNeg = new Repair(-1, 3, "X Force");
-	
-//	// add techs to queue
-//	techQ.add(tech1);
-//	techQ.add(tech2);
-//	techQ.add(tech3);
-//	techQ.add(tech4);
-//	techQ.add(tech5);
-//	techQ.add(tech6);
-//	
-//	// addition of repair objects to priority queue
-//	repairsNotStartedQ.add(repair1);
-//	repairsNotStartedQ.add(repair2);
-//	repairsNotStartedQ.add(repair3);
-//	repairsNotStartedQ.add(repair4);
-//	repairsNotStartedQ.add(repair5);
-//	repairsNotStartedQ.add(repair6);
-//	repairsNotStartedQ.add(repair0);
-//	repairsNotStartedQ.add(repairNeg);
-//		
-//		// open the MainWindow
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					MainWindow frame = new MainWindow();
-//					frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
 	
 	// private constructor to force usage of other constructor
 	private MainWindow() {
@@ -130,7 +57,6 @@ public class MainWindow extends JFrame {
 		
 		// create frame
 		JFrame mainFrame = new JFrame();
-		
 		mainFrame.setTitle("Repair Jobs");
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setBounds(100, 100, 436, 300);
@@ -154,8 +80,8 @@ public class MainWindow extends JFrame {
 		JButton btnAssignNextJob = new JButton("ASSIGN NEXT REPAIR");
 		btnAssignNextJob.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				assignTech(repairsNotStartedQ, techQ, inProgressList);
-				System.out.println("\"ASSIGN NEXT JOB\" clicked");
+			assignTech(repairsNotStartedQ, techQ, inProgressList);
+			//System.out.println("\"ASSIGN NEXT JOB\" clicked");
 			}
 		});
 		btnAssignNextJob.setBounds(32, 88, 162, 47);
@@ -165,7 +91,8 @@ public class MainWindow extends JFrame {
 		JButton btnCompleteRepair = new JButton("COMPLETE REPAIR");
 		btnCompleteRepair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("\"COMPLETE REPAIR\" clicked");
+				completeRepair(inProgressList, completedList, techQ);
+				//System.out.println("\"COMPLETE REPAIR\" clicked");
 			}
 		});
 		btnCompleteRepair.setBounds(226, 88, 162, 47);
@@ -176,10 +103,10 @@ public class MainWindow extends JFrame {
 		btnRepairsNotStarted.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		btnRepairsNotStarted.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("\"Repairs Not Started\" clicked");
 				RepairsNotStartedWindow rNSW = new RepairsNotStartedWindow(repairsNotStartedQ);
 				rNSW.setVisible(true);
-			}
+				//System.out.println("\"Repairs Not Started\" clicked");
+				}
 		});
 		btnRepairsNotStarted.setBounds(10, 210, 118, 40);
 		mainPanel.add(btnRepairsNotStarted);
@@ -188,7 +115,9 @@ public class MainWindow extends JFrame {
 		JButton btnRepairsInProgress = new JButton("Repairs In Progress");
 		btnRepairsInProgress.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("\"Repairs In Progress\" clicked");
+				InProgressWindow iPW = new InProgressWindow(inProgressLL);
+				iPW.setVisible(true);
+				//System.out.println("\"Repairs In Progress\" clicked");
 			}
 		});
 		btnRepairsInProgress.setFont(new Font("Tahoma", Font.PLAIN, 9));
@@ -199,14 +128,15 @@ public class MainWindow extends JFrame {
 		JButton btnCompletedRepairs = new JButton("Completed Repairs");
 		btnCompletedRepairs.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("\"Completed Repairs\" clicked");
+				CompletedRepairsWindow cRW = new CompletedRepairsWindow(completedLL);
+				cRW.setVisible(true);
+				//System.out.println("\"Completed Repairs\" clicked");
 			}
 		});
 		btnCompletedRepairs.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		btnCompletedRepairs.setBounds(288, 210, 118, 40);
 		mainPanel.add(btnCompletedRepairs);
 		
-		//mainFrame.add(mainPanel);
 		//set mainFrame to be visible
 		mainFrame.setVisible(true);
 
@@ -226,13 +156,12 @@ public class MainWindow extends JFrame {
 	static void assignTech(PriorityQueue<Repair> repairPQ, Queue<Technician> techQ, LinkedList<Repair> inProgressList) {
 		// check if queue has at least one technician, if not, then open no tech available window
 		if (techQ.size() <= 0) {
-			NoTechsDialog nTD = new NoTechsDialog();
-			nTD.setVisible(true);
+			JOptionPane.showMessageDialog(null, "No Techs Available");
 			return;
+			
 		// check if repairPQ has at least one repair, if not, then open no repair available window
 		} else if (repairPQ.isEmpty()) {
-			NoRepairsDialog nRD = new NoRepairsDialog();
-			nRD.setVisible(true);
+			JOptionPane.showMessageDialog(null, "No Repairs Avaliable");
 			return;
 		}
 		Repair current = repairPQ.poll();
@@ -252,29 +181,45 @@ public class MainWindow extends JFrame {
 	 * @param completedList - LinkedList of completed repairs
 	 * @param techQ - queue data structure containing available technicians
 	 */
-	static void completeRepair(LinkedList<Repair> inProgressList, int orderNum, LinkedList<Repair> completedList, Queue<Technician> techQ) {
-		// check if inProgressList has at least one element in it, if not, then open no repairs in progress window
-		if (inProgressList.size() <= 0) {
-			System.out.println("No repairs are in progress WINDOW");
+	static void completeRepair(LinkedList<Repair> inProgressList, LinkedList<Repair> completedList, Queue<Technician> techQ) {
+		// check if inProgressList has at least one element in it, if not, then display message dialog
+		if (inProgressList.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "There are no repairs currently in progress");
 			return;
 		}
 		
-		// check each element in inProgressList for matching orderNum
-		for (Repair repairs : inProgressList) {
-			if(repairs.getOrderNum() == orderNum) {
-				Repair current = repairs;
-				current.setCompletionDate(LocalDate.now());
-				Technician currentTech = current.getTech();
-				techQ.add(currentTech);
-				completedList.add(current);
-				inProgressList.remove(current);
-				System.out.println("Order number: " + current.getOrderNum() + ", completed on " + current.getCompletionDate() + " by " + current.getTech().getFullName() + " WINDOW");
-				return;
+		boolean validInput = false;
+		String orderNumInput = JOptionPane.showInputDialog("Enter the order number you wish to complete: ", "Order Number");
+		
+		while (!validInput && orderNumInput != null) {
+			try {
+				if (Integer.parseInt(orderNumInput) <= 0) {
+					orderNumInput = JOptionPane.showInputDialog("INVALID ENTRY! Order Number to Complete: ", "Order Number");
+				} else {
+					// check each element in inProgressList for matching orderNum
+					for (Repair repairs : inProgressList) {
+						if(repairs.getOrderNum() == Integer.parseInt(orderNumInput)) {
+							Repair current = repairs;
+							current.setCompletionDate(LocalDate.now());
+							Technician currentTech = current.getTech();
+							techQ.add(currentTech);
+							completedList.add(current);
+							inProgressList.remove(current);
+							JOptionPane.showMessageDialog(null, "Order number: " + current.getOrderNum() + ", completed on " + current.getCompletionDate() + " by " + current.getTech().getFullName());
+							System.out.println("Order number: " + current.getOrderNum() + ", completed on " + current.getCompletionDate() + " by " + current.getTech().getFullName() + " WINDOW");
+							validInput = true;
+							return;
+						} 
+					}
+					// message dialog indicating the order number was not found in the inProgressList
+					JOptionPane.showMessageDialog(null, "Order number not found");
+					System.out.println("Order Number Not Found WINDOW");
+					break;
+				}
+			} catch (NumberFormatException ex) {
+				orderNumInput = JOptionPane.showInputDialog("INVALID ENTRY! Order Number to Complete: ", "Order Number");
 			}
 		}
 		
-		// message/window indication the order number was not found in the inProgressList
-		System.out.println("Order Number Not Found WINDOW");
-		return;
 	}
 }
