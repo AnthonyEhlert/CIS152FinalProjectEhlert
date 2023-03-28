b384a28707c38e65986dc922385e04418d8a0ee9
Tony Ehlert <aehlert@dmacc.edu>
2023-03-28 10:27:46 -0500

    Update RepairsNotStartedWindow.java
    Changed textArea box contents to display more readable data

--- src/view/RepairsNotStartedWindow.java
+++ src/view/RepairsNotStartedWindow.java
@@ -70,7 +70,7 @@ public class RepairsNotStartedWindow extends JFrame {
 			pQueueContents = "All repairs are either in progress or have been completed";
 		} else {
 			for (Repair repairs : repairsPQ) {
-				pQueueContents += (repairs.toString() + "\n");
+				pQueueContents += ("Priority Value: " + repairs.getPriority() + ", Order Number: " + repairs.getOrderNum() + ", Customer: " + repairs.getCustomer() + "\n");
 			}
 		}
 		textArea.append(pQueueContents);
